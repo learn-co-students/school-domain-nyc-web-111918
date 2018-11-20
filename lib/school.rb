39d7cecf2b985a5b86651f27d38066822fe03be4
Chris Benedict <chrisjbenedict@gmail.com>
2018-11-20 16:20:06 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
 # code here!
+class School
+
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    if !roster[grade]
+      roster[grade] = []
+      roster[grade] << student_name
+    else
+      roster[grade] << student_name
+    end
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    # create a new hash
+    ordered = {}
+    # iterate over each grade in the roster
+    roster.map do |grade, students|
+      # set keys in ordered equal to sorted strings of students from roster
+      ordered[grade] = students.sort
+    end
+    # return ordered
+    ordered
+  end
+
+end
