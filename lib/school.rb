a68f09819efd4d83f0989727b1a685d0f3cb34e7
Tonyjlum <tonyjlum@gmail.com>
2018-11-20 16:36:55 -0500

    School 'class'

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,24 @@
-# code here!
+require "pry"
+
+class School
+  attr_accessor :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    @roster[grade] = [] if @roster[grade].nil?
+    @roster[grade] << student
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each { |grade, students| students.sort! }
+  end
+
+end
