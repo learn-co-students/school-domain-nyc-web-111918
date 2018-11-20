57566c9ef530d8d34d6305bb5d65e5f1c09f8a58
lumrachele <rlum894@gmail.com>
2018-11-20 18:53:15 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+  attr_accessor :roster
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if roster.include?(grade)
+      roster[grade]<< name
+    else
+    roster[grade]= [] #this will be an else statement, if the key :grade does not already exist
+    roster[grade]<< name #add name to array
+    end
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    roster.each do |grade, names|
+      roster[grade] = names.sort
+    end
+  end
+end
